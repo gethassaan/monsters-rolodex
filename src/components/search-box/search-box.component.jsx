6fbc4ef9f2640fad9c1ffd9321eaef2ff2ac63e6
Hassaan <hassaan@rebeltechnology.io>
2019-08-06 17:49:16 +0500

    added search box field

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './search-box.style.css';
+
+export const SearchBox = ({placeholder, handleChange}) => (
+    <div>
+        <input className="search" type="search" placeholder={placeholder} onChange={handleChange}></input>
+    </div>
+)
