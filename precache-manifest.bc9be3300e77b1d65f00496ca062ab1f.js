5ec2b418a38fc6eaca0724ec1e5a3b58f46c4188
Hassaan <hassaaanahmedsiddiqui@gmail.com>
2019-08-07 08:41:31 +0500

    Updates

---
+++ precache-manifest.bc9be3300e77b1d65f00496ca062ab1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029ef31e8971363ae4906f1b1483e2e9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3f117390f43dc486ffdc",
+    "url": "/monsters-rolodex/static/css/main.6ebb84ad.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3f117390f43dc486ffdc",
+    "url": "/monsters-rolodex/static/js/main.c04cd6d8.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
