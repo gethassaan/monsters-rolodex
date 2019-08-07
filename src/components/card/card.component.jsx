e235387fcdca0b7ee4c26abbc0e59ec2cf6a38f2
Hassaan <hassaaanahmedsiddiqui@gmail.com>
2019-08-07 08:03:51 +0500

    completed design of app

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ import "./card.style.css";
 export const Card = props =>    (
     <div className='card-container'>
         <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster"/>
-        <h1>{props.monster.name}</h1>
+        <h2>{props.monster.name}</h2>
         <h3>{props.monster.email}</h3>
     </div>
 );
